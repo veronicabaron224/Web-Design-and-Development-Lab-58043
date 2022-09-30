3a17f85c9a90d66a3bd667564efa1bad5f0e3198
Ronnie <77873456+veronicabaron224@users.noreply.github.com>
2022-09-30 09:43:05 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,18 @@
+<html>
+	<head><title>Exercise 1</title></head>
+	<body bgcolor = "pink">
+	<?php
+		$fullname = "Veronica Frances S. Baron";
+		echo $fullname;
+		echo "<br>";
+		$collegeprogram = "BS Computer Engineering";
+		echo $collegeprogram;
+		echo "<br>";
+		$yearandsection = "3rd Year, CPE 301";
+		echo $yearandsection;
+		echo "<br>";
+		$string = "Today, I will learn PHP programming";
+		echo $string;
+	?>
+</body>
+</html>
