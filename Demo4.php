da483b7594e3164ebbb29fd78b615326055c816d
Ronnie <77873456+veronicabaron224@users.noreply.github.com>
2022-10-23 00:40:40 +0800

    Add files via upload

---
+++ Demo4.php
@@ -0,0 +1,68 @@
+<html>
+	<head><title>My Looping Statements</title></head>
+	<?php
+
+	$fuel = 10;
+
+	/*
+	//while loop
+	while ($fuel>1)
+	{
+		echo "There's enough fuel!";
+		--$fuel;
+	}
+
+	//do...while
+	$count = 1;
+	do
+		echo "$count times 12 id ". $count*12 . "<br>";
+	while(++$count<=12);
+
+	do
+	{
+		echo "There's enough fuel! <br>";
+	}	
+	while(--$fuel>1)
+
+	//for loop
+	for($fuel=10; $fuel>=1; --$fuel)
+	{
+		echo "There's enough fuel <br>";
+	}
+
+	//foreach
+	$colors = array("red","green","blue");
+	foreach($colors as $value)
+	{
+			echo "$value <br>";
+	}
+
+	//break statement
+	for($x=0; $x<10; $x++)
+	{
+			if($x==4)
+			{
+				echo "The number is $x <br>";
+				break;
+			}
+	}
+
+	//continue
+	for($x=0; $x<10; $x++)
+	{
+			if($x==4)
+			{
+				continue;
+			}
+		echo "The number is $x <br>";
+	}
+	*/
+
+	//Exercise: Let's Try
+	for($x=9; $x>2; $x--)
+	{
+		echo "$x <br>";
+	}
+
+	?>
+</html>
